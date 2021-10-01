327e9fa6cb9625ab5d039503b1e6ee0916ec0c1f
Gabriel Batalha <71796398+GabrielBatalhaDEV@users.noreply.github.com>
2021-10-01 09:57:18 -0300

    remove local database
    remove files: [database.sqlite, index.ts]
    path: src/database/[database.sqlite, index.ts]
    
    modefied server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,29 @@
-import express, { NextFunction, Request, Response } from 'express'
-import 'express-async-errors'
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
-import 'reflect-metadata'
-import './database'
-import { router } from './routes'
-const app = express()
+import "reflect-metadata";
+import { router } from "./routes";
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(router)
+app.use(router);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
-    if(err instanceof Error){
-         return response.status(400).json({
-              error: err.message
-         })
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
     }
 
     return response.status(500).json({
-         status: "error",
-         message: "Internal Server Error"
-    })
-})
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
 
-app.listen(3000,()=>{
-    console.log('Server Running');
-})
+app.listen(3000, () => {
+  console.log("Server Running");
+});
