8c329bac0f7dc59209279952e920c4b08cd9899f
Gabriel Batalha <71796398+GabrielBatalhaDEV@users.noreply.github.com>
2021-10-01 10:02:04 -0300

    fix database local
    recovered file: index.ts
                     path: src/database/index.ts
    
    modified server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
 import "reflect-metadata";
+import "./database";
 import { router } from "./routes";
 const app = express();
 
