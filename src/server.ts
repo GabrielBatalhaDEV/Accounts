77a56bd715c88e40e22ab80f0eecfd727bff8d60
Gabriel Batalha <71796398+GabrielBatalhaDEV@users.noreply.github.com>
2021-10-05 20:04:12 -0300

    add cors package

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import "express-async-errors";
 import "reflect-metadata";
 import "./database";
 import { router } from "./routes";
+import cors from "cors";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
 
 app.use(
